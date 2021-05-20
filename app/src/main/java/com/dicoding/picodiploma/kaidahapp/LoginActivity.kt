c49806d786e3331f8bb2845294f656b80cc3ceb3
MuhamadNabilRabani <A0060637@bangkit.academy>
2021-05-20 14:46:36 +0700

    Mobile

--- app/src/main/java/com/dicoding/picodiploma/kaidahapp/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/kaidahapp/LoginActivity.kt
@@ -2,22 +2,33 @@ package com.dicoding.picodiploma.kaidahapp
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.picodiploma.kaidahapp.databinding.ActivityLoginBinding
+import com.dicoding.picodiploma.kaidahapp.helper.SharedPreference
 
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var sharedPreference: SharedPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sharedPreference = SharedPreference(this)
+        val isSignedin = sharedPreference.getValueBoolien("signed", false)
+
+        if (isSignedin) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         val animationDrawable = binding.abc.background as AnimationDrawable
         animationDrawable.setEnterFadeDuration(2000)
@@ -35,8 +46,12 @@ class LoginActivity : AppCompatActivity() {
 
     private fun login(context: Context, username: String, password: String) {
         Log.d("checkParam2", "Username: ${username} | Password: ${password}")
-        if (username == "coba"){
-            if (password == "coba"){
+        if (username == "coba") {
+            if (password == "coba") {
+                sharedPreference.save("username", username)
+                sharedPreference.save("password", password)
+                sharedPreference.save("signed", true)
+
                 val intent = Intent(context, MainActivity::class.java)
                 startActivity(intent)
                 Toast.makeText(context, "Login Berhasil", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/dicoding/picodiploma/kaidahapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/kaidahapp/MainActivity.kt
@@ -2,10 +2,74 @@ package com.dicoding.picodiploma.kaidahapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.dicoding.picodiploma.kaidahapp.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.kaidahapp.fragment.FollowedFragment
+import com.dicoding.picodiploma.kaidahapp.fragment.HomeFragment
+import com.dicoding.picodiploma.kaidahapp.fragment.ProfileFragment
+import com.dicoding.picodiploma.kaidahapp.helper.SharedPreference
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreference: SharedPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreference = SharedPreference(this)
+
+        val fragOne = HomeFragment()
+        val fragTwo = FollowedFragment()
+        val fragThree = ProfileFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_fragment, fragOne, "FragmentHome")
+            commit()
+        }
+
+        binding.fHome.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_fragment, fragOne, "FragmentHome")
+                commit()
+            }
+        }
+
+        binding.fFollowed.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_fragment, fragTwo, "FragmentFollowed")
+                commit()
+            }
+        }
+
+        binding.fProfile.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_fragment, fragThree, "FragmentProfile")
+                commit()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        AlertDialog.Builder(this@MainActivity).setTitle("Keluar Aplikasi").setMessage("Apakah anda ingin menutup aplikasi?").setPositiveButton(R.string.ya) {
+            dialog, whichButton -> super.onBackPressed()
+            finishAffinity()
+        } .setNegativeButton(R.string.tidak) {
+            dialog, whichButton -> //Close
+        } .show()
+//        val currentFragment = this@MainActivity.supportFragmentManager.findFragmentByTag("FragmentHome")
+//        if(currentFragment is HomeFragment) {
+//            AlertDialog.Builder(this@MainActivity).setTitle("Keluar Aplikasi").setMessage("Apakah anda ingin menutup aplikasi?").setPositiveButton(R.string.ya) {
+//                dialog, whichButton -> super.onBackPressed()
+//            } .setNegativeButton(R.string.tidak) {
+//                dialog, whichButton -> //Close
+//            } .show()
+//        }
+//        else{
+//            super.onBackPressed()
+//        }
     }
 }
